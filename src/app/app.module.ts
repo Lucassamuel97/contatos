ed19d657cd5ed8988d705cfee77332264fbd80dc
Lucassamuel97 <lukassamuka88@gmail.com>
2019-08-22 20:17:05 -0300

    create inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { ContactsListPage } from '../pages/contacts-list/contacts-list';
+import { CreateContactPage } from '../pages/create-contact/create-contact';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { ContactsProvider } from '../providers/contacts/contacts';
 
 @NgModule({
   declarations: [
@@ -17,10 +23,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    ContactsListPage,
+    CreateContactPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -29,12 +39,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    ContactsListPage,
+    CreateContactPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactsProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/create-contact/create-contact.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { ContactsProvider } from '../../providers/contacts/contacts';
+
+/**
+ * Generated class for the CreateContactPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-create-contact',
+  templateUrl: 'create-contact.html',
+})
+export class CreateContactPage {
+  model: Contact;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toast: ToastController, public contactsProvider: ContactsProvider) {
+    this.model = new Contact();
+    this.model.name = 'Novo contato';
+    this.model.gender = 'male';
+    this.model.birthday = '1997-08-12';
+    this.model.employed = false;
+  }
+
+  createContact() {
+    var data = {
+      'contact': {
+        'name': this.model.name,
+        'gender': this.model.gender,
+        'birthday': this.model.birthday,
+        'employed': this.model.employed,
+      }
+    };
+    this.contactsProvider.addContact(data)
+      .then((result: any) => {
+        this.toast.create({ message: 'Contato criado'}).present();
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Falha ao criar o contato: ' + error.error }).present();
+        console.log(error);
+      });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CreateContactPage');
+  }
+
+}
+
+export class Contact {
+  name: string;
+  gender: string;
+  birthday: string;
+  employed: false;
+}
